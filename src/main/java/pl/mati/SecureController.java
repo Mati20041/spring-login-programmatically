aba5d0659aeef39e1e370231d0755c66b26bd037
Mati <mat20041@gmail.com>
2017-02-01 20:28:35 +0100

    extracted login controller

--- src/main/java/pl/mati/SecureController.java
+++ src/main/java/pl/mati/SecureController.java
@@ -1,36 +1,11 @@
 package pl.mati;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecureController {
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @RequestMapping("/login")
-    public void login() {
-        try {
-            SecurityContextHolder.getContext().setAuthentication(authenticationManager.authenticate(new UsernamePasswordAuthenticationToken("mati", "mati")));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @RequestMapping("/wrong-login")
-    public void wrongLogin() {
-        try {
-            SecurityContextHolder.getContext().setAuthentication(authenticationManager.authenticate(new UsernamePasswordAuthenticationToken("mati", "notmati")));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     @RequestMapping("/secure")
     public String hi(Authentication authentication) {
